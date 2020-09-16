9c6ac4c8497cd100fbbfd4a458ac13935c5c1025
tuh00941 <37967710+tuh00941@users.noreply.github.com>
2020-09-16 02:35:09 -0400

    Added a button that can change the TextView text.

--- src/main/java/edu/temple/helloworld/MainActivity.java
+++ src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.textView);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(getString(R.string.button_clicked));
+            }
+        });
     }
 }
